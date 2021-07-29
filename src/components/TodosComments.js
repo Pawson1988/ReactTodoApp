2cda7a6d0b140b5a7349db2739ad479f8dc96dc7
Pawson1988 <pawson1988@me.com>
2021-07-29 22:44:37 +0200

    added handle change for comment input - reset value

--- src/components/TodosComments.js
+++ src/components/TodosComments.js
@@ -2,17 +2,23 @@ import React, { useState } from "react";
 
 const TodosComments = () => {
   const [comment, setComment] = useState("No Comment");
+  const [inputField, setInputField] = useState("");
 
   function onSubmitComment(e) {
     setComment(e.target.commentInput.value);
+    setInputField("");
     e.preventDefault();
+  }
+
+  function handleChange(e) {
+    setInputField(e.target.value);
   }
 
   return (
     <div>
       <p>{comment}</p>
       <form onSubmit={onSubmitComment}>
-        <input name="commentInput" type="text"></input>
+        <input onChange={handleChange} value={inputField} name="commentInput" type="text"></input>
         <button>Add Comment</button>
       </form>
     </div>
